7094424274944e66b3111fd42456b7a096756c30
Arthur Otaviano <oarthurotaviano@gmail.com>
2026-09-02 10:12:52 -0300

    chore: simplify site url configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,8 @@ const notoSans = Noto_Sans({
 
 const title = 'Larissa Machado'
 const description = 'Designer Gráfico Senior'
-
 export const metadata: Metadata = {
-  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL ?? (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000')),
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL!),
   title,
   description,
   openGraph: {
